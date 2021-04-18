15125ec37fda60ec61881adcc6126addf0755a33
40155060 <72089156+40155060@users.noreply.github.com>
2021-04-19 00:39:19 +0100

    Added the modal

--- main.js
+++ main.js
@@ -1,4 +1,26 @@
+// Index for slide show
 var slideIndex = 0;
+
+// Get the modal of first advertisement
+var modal1 = document.getElementById("addinfo_1");
+
+// Get the button of first advertisement that open the modal
+var btn1 = document.getElementById("addbtn_1");
+
+
+// Get the modal of second advertisement.
+var modal2 = document.getElementById("addinfo_2");
+
+// Get the button of second avdertisement that open the modal
+var btn2 = document.getElementById("addbtn_2");
+
+// Get the <span> elements that closes the modal for first advertisement
+var span1 = document.getElementsByClassName("close")[0];
+
+// Get the <span> elements that closes the modal for second advertisement
+var span2 = document.getElementsByClassName("close")[1];
+
+
 showSlides();
 
 function showSlides() {
@@ -16,4 +38,39 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
   setTimeout(showSlides, 4000); // Change image every 2 seconds
+}
+
+
+// When the user clicks the button of first advertisement, open the modal 
+btn1.onclick = function() {
+  modal1.style.display = "block";
+}
+
+// When the user clicks the button of second advertisement, open the modal 
+btn2.onclick = function() {
+  modal2.style.display = "block";
+}
+
+// When the user clicks on <span> (x) first advertisement, close the modal
+span1.onclick = function() {
+  modal1.style.display = "none";
+}
+
+// When the user clicks on <span> (x) of second advertisement, close the modal
+span2.onclick = function() {
+  modal2.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal of first advertisement, close it
+window.onclick = function(event) {
+  if (event.target == modal1) {
+    modal1.style.display = "none";
+  }
+}
+
+// When the user clicks anywhere outside of the modal of second advertisement, close it
+window.onclick = function(event) {
+  if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
 }
